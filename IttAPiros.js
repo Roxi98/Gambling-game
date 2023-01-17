4e865485e16fd36858ed9f3cec7109456141f687
Roxi98 <116589716+Roxi98@users.noreply.github.com>
2023-01-17 19:48:20 +0100

    finished game

---
+++ IttAPiros.js
@@ -0,0 +1,34 @@
+const { randomIntBetweenTwoNumbers } = require("../XO/funkciok");
+const readline = require("readline");
+
+const { count } = require("console");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let Doors = [1, 2, 3];
+let WinningDoor = randomIntBetweenTwoNumbers(0, 2);
+Doors[WinningDoor] = "Fcking new car";
+
+function gameMode(end) {
+  return new Promise((resolve, reject) => {
+    rl.question("Please choose a door between 1-3  ", (answer) => {
+      if (Doors[answer - 1] === "Fcking new car") {
+        console.log("You win!");
+        resolve(false);
+      } else {
+        console.log("You loose,give me your money");
+      }
+      resolve(true);
+    });
+  });
+}
+async function game() {
+  let end = false;
+  do end = await gameMode();
+  while (end === true);
+  rl.close();
+}
+
+game();
